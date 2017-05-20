9c4a74e60e7d84c1e37e0933a776dc8750bbc957
Daniel <wilkowski.kontakt@gmail.com>
2017-05-20 18:29:07 +0200

    Removed printf()

--- tree.cpp
+++ tree.cpp
@@ -125,7 +125,7 @@ struct Tree {
             printNodes(start->leftChild);
         }
 
-        printf("%d\n", start->value);
+        std::cout << start->value << ", ";
 
         if (start->rightChild != NULL) {
             printNodes(start->rightChild);
@@ -158,7 +158,7 @@ int randomBetween(int bound1, int bound2) {
 int main() {
     Tree *tree = new Tree();
 
-    printf("Podaj ilosc wzlow: ");
+    std::cout << "Podaj ilosc wzlow: ";
     int size;
     scanf("%d", &size);
 
@@ -166,18 +166,18 @@ int main() {
         int randomValue = randomBetween(1, 100);
         tree->addValue(randomValue);
     }
-    printf("\n");
+    std::cout << std::endl;
 
     tree->printNodes();
     tree->visualize();
 
-    printf("Wartość węzła do usunięcia: \n");
+    std::cout << "Wartość węzła do usunięcia: ";
 
     int k;
     scanf("%d", &k);
     tree->removeNode(tree->findNodeByValue(k));
 
-    printf("\n\n");
+    std::cout << std::endl << std::endl;
 
     tree->printNodes();
     tree->visualize();
