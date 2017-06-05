b9a2f777b2e0620ef9a384325a578cc083a27d4a
Daniel <wilkowski.kontakt@gmail.com>
2017-06-05 10:40:07 +0200

    Added interactive menu

--- heap.cpp
+++ heap.cpp
@@ -4,6 +4,7 @@
 
 struct Heap {
 private:
+    int capacity;
     std::vector<int> elements;
 
     // zwroc rodzica elementu elements[i]
@@ -65,6 +66,13 @@ private:
     }
 
 public:
+    Heap(int _capacity) {
+        if (_capacity < 0) {
+            _capacity = 0;
+        }
+        capacity = _capacity;
+    }
+
     // zwroc rozmiar kopca
     unsigned int size() {
         return elements.size();
@@ -77,11 +85,13 @@ public:
 
     // dodaj wartosc klucza do kopca
     void insert(int key) {
-        // dodaj nowy element na koniec vectora
-        elements.push_back(key);
+        if (size() < capacity) {
+            // dodaj nowy element na koniec vectora
+            elements.push_back(key);
 
-        // uzyj get element oraz wywołaj heapify_up()
-        heapify_up(size() - 1);
+            // uzyj get element oraz wywołaj heapify_up()
+            heapify_up(size() - 1);
+        }
     }
 
     // funkcja usuwajaca najwyzszy element
@@ -105,55 +115,91 @@ public:
 };
 
 int main() {
-    Heap heap;
+    Heap *heap = NULL;
 
-    heap.insert(3);
-    heap.insert(2);
-    heap.insert(15);
+    while (true) {
+        std::cout << "Dostepne opcje: " << std::endl;
 
-    std::cout << "Size is " << heap.size() << std::endl;
+        if (heap == NULL) {
+            std::cout << "1) Stworz kopiec" << std::endl;
+            std::cout << "2) Wyjdz z programu" << std::endl;
+        } else {
+            std::cout << "1) Dodaj liczbe" << std::endl;
+            std::cout << "2) Pokaz gorny element" << std::endl;
+            std::cout << "3) Usun gorny element" << std::endl;
+            std::cout << "4) Pokaz rozmiar kopca" << std::endl;
+            std::cout << "5) Czy kopiec pusty" << std::endl;
+            std::cout << "6) Usun kopiec" << std::endl;
+            std::cout << "7) Wyjdz z programu" << std::endl;
+        }
 
-    std::cout << heap.top() << " ";
-    heap.pop();
+        std::cout << "Podaj opcje: ";
+        int choice;
+        std::cin >> choice;
 
-    std::cout << heap.top() << " ";
-    heap.pop();
+        if (heap == NULL) {
+            switch (choice) {
+                case 1:
+                    std::cout << "Podaj nowy rozmiar kopca: ";
+                    int size;
+                    std::cin >> size;
+                    heap = new Heap(size);
+                    break;
 
-    heap.insert(5);
-    heap.insert(4);
-    heap.insert(45);
+                case 2:
+                    delete heap;
+                    return 0;
 
-    std::cout << std::endl << "Size is " << heap.size() << std::endl;
+                default:
+                    std::cout << "Niepoprawna opcja" << std::endl;
+            }
+        } else {
+            switch (choice) {
+                case 1:
+                    std::cout << "Podaj liczbe do dodania: ";
+                    int number;
+                    std::cin >> number;
+                    heap->insert(number);
+                    break;
+                case 2:
+                    try {
+                        int top = heap->top();
+                        std::cout << "gorny element to '" << top << "'" << std::endl;
+                    }
+                    catch (std::out_of_range &e) {
+                        std::cout << "Blad z funkcja wyciaganiem najwyzszczego elementu" << std::endl;
+                    }
+                    break;
+                case 3:
+                    try {
+                        heap->pop();
+                        std::cout << "gorny element usuniety" << std::endl;
+                    }
+                    catch (std::out_of_range &e) {
+                        std::cout << "Blad z funkcja usuwania najwyzszczego elementu" << std::endl;
+                    }
+                    break;
 
-    std::cout << heap.top() << " ";
-    heap.pop();
+                case 4:
+                    std::cout << "Rozmiar kopca to: " << heap->size() << " elementow" << std::endl;
+                    break;
+                case 5:
 
-    std::cout << heap.top() << " ";
-    heap.pop();
+                    std::cout << "Czy kopiec pusty: " << (heap->empty() ? "tak" : "nie") << std::endl;
+                    break;
 
-    std::cout << heap.top() << " ";
-    heap.pop();
+                case 6:
+                    delete heap;
+                    heap = NULL;
+                    break;
 
-    std::cout << heap.top() << " ";
-    heap.pop();
+                case 7:
+                    delete heap;
+                    return 0;
 
-    std::cout << std::endl << std::boolalpha << heap.empty() << std::endl;
-
-    try {
-        // zwroc najmniejszy element
-        heap.top();
+                default:
+                    std::cout << "Niepoprawna opcja" << std::endl;
+            }
+        }
     }
-    catch (std::out_of_range &e) {
-        std::cout << "Blad z funkcja wyciaganiem najwyzszczego elementu";
-    }
-
-    try {
-        // usun najwyzszy element
-        heap.pop();
-    }
-    catch (std::out_of_range &e) {
-        std::cout << "Error: " << e.what();
-    }
-
-    return 0;
 }
